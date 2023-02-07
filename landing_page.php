9b10ee63d56076ab112ce8e79b8e1ecbeb640cf3
DonGuillotine <infect3dlab@gmail.com>
2023-02-07 12:25:32 +0100

    Reading Items from Database

---
+++ landing_page.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+        table tr td:last-child{
+            width: 120px;
+        }
+    </style>
+    <script>
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();   
+        });
+    </script>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mt-5 mb-3 clearfix">
+                        <h2 class="pull-left">Employees Details</h2>
+                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>
+                    </div>
+            <?php 
+                // Include configuration file
+                require_once "config.php";
+
+                // Attempt select query execution
+                $sql = "SELECT * FROM employees";
+                if($result = mysqli_query($connection, $sql)){
+                    if(mysqli_num_rows($result) > 0){
+                        echo '<table class="table table-bordered table-striped">';
+                            echo '<thead>';
+                                echo '<tr>';
+                                    echo '<th>ID</th>';
+                                    echo '<th>Name</th>';
+                                    echo '<th>Address</th>';
+                                    echo '<th>Salary</th>';
+                                    echo '<th>Action</th>';
+                                echo '</tr>';
+                            echo '</thead>';
+                            echo '<tbody>';
+                            while($row = mysqli_fetch_array($result)){
+                                echo '<tr>';
+                                    echo '<td>' . $row['id'] . '</td>';
+                                    echo '<td>' . $row['name'] . '</td>';
+                                    echo '<td>' . $row['address'] . '</td>';
+                                    echo '<td>' . $row['salary'] . '</td>';
+                                    echo '<td>';
+                                        echo '<a href="" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                        echo '<a href="" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                        echo '<a href="" class="mr-3" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                    echo '</td>';
+                                echo '</tr>';
+                            }
+                            echo '</tbody>';
+                        echo '</table>';
+                        // Free memory associated with result
+                        mysqli_free_result($result);
+                    } else{
+                        echo '<div class="alert alert-danger"><em>No Reults Found!</em></div>';
+                    }
+                }else{
+                    echo "Something went wrong 😭";
+                }
+                // Close connection
+                mysqli_close($connection);
+            ?>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
