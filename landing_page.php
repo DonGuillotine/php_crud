e421d9ec93f99910c8c9c0e6449924832c349de2
Donald Nwokoro <infect3dlab@gmail.com>
2023-02-21 11:06:42 +0100

    Update and Delete

--- landing_page.php
+++ landing_page.php
@@ -59,8 +59,8 @@
                                     echo '<td>' . $row['salary'] . '</td>';
                                     echo '<td>';
                                         echo '<a href="read.php?id='. $row['id'] . '" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                                        echo '<a href="" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                                        echo '<a href="" class="mr-3" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                        echo '<a href="update.php?id='. $row['id']. '" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                        echo '<a href="delete.php?id='. $row['id']. '" class="mr-3" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
                                     echo '</td>';
                                 echo '</tr>';
                             }
